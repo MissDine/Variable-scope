869f8c63cc221894e86e488fd6e1d23cf355330d
MissDine <enidnyatichi1212@gmail.com>
2022-06-07 11:08:18 +0300

    feature edit

---
+++ app.rb
@@ -0,0 +1,4 @@
+# Local variables: must start with a lowercase letter or an underscore (_): name = "Lucy"
+# Global variables: must start with a dollar sign ($): $name = "Lucy"
+# Instance variables: must start with an at symbol (@) @name = "Lucy")
+# Class variables: must start with two at symbols (@@) (@@name = "Lucy")g
